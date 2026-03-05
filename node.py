54cf0b44ebdc6a63cd58ee1aa86b8dc9eaccd23b
suhyab <shaik.suhyab3@gmail.com>
2026-03-05 15:27:47 -0600

    resolved naming conflicts

--- node.py
+++ node.py
@@ -1,13 +1,14 @@
 class Node:
-    def __init__(self, id, centralHubs, bagOfWords):
+    def __init__(self, id, central_hub, bag_of_words):
         self.id = id
-        self.centralHubs = centralHubs
-        self.bagOfWords = bagOfWords
+        self.central_hub = central_hub
+        self.bag_of_words = bag_of_words
         self.active = True
         self.neighbors = []
+        self.message_queue = []
 
         self.get_new_neighbors()
 
     def get_new_neighbors(self):
-        self.neighbors = self.centralHubs.get_neighbors(self.id, self.neighbors)
+        self.neighbors = self.central_hub.get_neighbors(self.id, self.neighbors)
 
