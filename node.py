70c026b2cc0a0769004d78be2d5a1eaab5e4f958
Sarthak1812 <sarthakmusmade@gmail.com>
2026-03-05 15:25:19 -0600

    functions

--- node.py
+++ node.py
@@ -1,13 +1,20 @@
+import random
+
 class Node:
+    MAX_ACTIONS_PER_TURN = 10 # TODO: Change this
     def __init__(self, id, centralHubs, bagOfWords):
         self.id = id
         self.centralHubs = centralHubs
         self.bagOfWords = bagOfWords
         self.active = True
         self.neighbors = []
+        self.action_this_turn = 0
 
         self.get_new_neighbors()
 
     def get_new_neighbors(self):
         self.neighbors = self.centralHubs.get_neighbors(self.id, self.neighbors)
 
+    
+    def takeTurn():
+        active_probab = random.random()
