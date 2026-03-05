0c7be9daddc6c429d17bbcde248788e85035ede9
ChanceBowlinger <125504917+ChanceBowlinger@users.noreply.github.com>
2026-03-05 16:25:03 -0600

    Added random word bag functionality

--- experiment_config.py
+++ experiment_config.py
@@ -4,6 +4,11 @@ def get_experiment_config():
         "num_nodes": 100,
         "num_actions_per_turn": 5,
         "num_turns": 100,
-        "min_neighbors": 3
+        "min_neighbors": 3,
+        "known_words_per_node": 5
     }
     return config
+
+def get_known_words():
+    # return known list of 100 words for bag of words representation
+    return [f"word_{i}" for i in range(100)]
